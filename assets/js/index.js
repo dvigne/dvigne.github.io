a4ce02d4cfdf8e22b8d579cd969b873bc066d7c9
Derick Vigne <derickvigne@me.com>
2016-07-30 18:49:41 -0400

    Added FitText Wait For Document Load

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,33 @@
 $(function() {
   $(document).ready(function (e) {
-
-    $('h1').fitText(
-      1.2, {
-        minFontSize: '35px',
-        maxFontSize: '65px'
-      }
-    );
     $(window).on('load', function(e) {
-      window.sr = ScrollReveal();
-      sr.reveal('.reveal', {
-        duration: 700,
-        delay: 500,
-        viewFactor: 1,
-        scale: 0.3,
-        distance: '0px'
-      }, 700)
-      sr.reveal('.about-text', {
-        duration: 600,
-        viewFactor: .5,
-        scale: 0.3,
-        distance: '0px',
-      }, 200);
-      sr.reveal('.support-text', {
-        duration: 600,
-        viewFactor: .5,
-        scale: 0.3,
-        distance: '0px',
-      }, 200);
+      $('.reveal').css('display', 'block');
+        window.sr = ScrollReveal();
+        sr.reveal('.reveal', {
+          duration: 700,
+          delay: 500,
+          viewFactor: 1,
+          scale: 0.3,
+          distance: '0px'
+        }, 700)
+        sr.reveal('.about-text', {
+          duration: 600,
+          viewFactor: .5,
+          scale: 0.3,
+          distance: '0px',
+        }, 200);
+        sr.reveal('.support-text', {
+          duration: 600,
+          viewFactor: .5,
+          scale: 0.3,
+          distance: '0px',
+        }, 200);
+        $('h1').fitText(
+          1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+          }
+        );
     });
   });
 });
