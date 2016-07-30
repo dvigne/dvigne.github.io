c18b1feb89a21f866cfe838456d3c2c7efe28c9c
Derick Vigne <derickvigne@me.com>
2016-07-30 15:13:17 -0400

    Added Wait For All HTML Elements To Load In Jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,21 +7,21 @@ $(function() {
         maxFontSize: '65px'
       }
     );
-
-    window.sr = ScrollReveal();
-    sr.reveal('.reveal', {
-      duration: 700,
-      delay: 500,
-      viewFactor: 1,
-      scale: 0.3,
-      distance: '0px'
-    }, 700)
-    sr.reveal('.about-text', {
-      duration: 600,
-      viewFactor: .5,
-      scale: 0.3,
-      distance: '0px',
-    }, 200);
-
+    $(window).on('load', function(e) {
+      window.sr = ScrollReveal();
+      sr.reveal('.reveal', {
+        duration: 700,
+        delay: 500,
+        viewFactor: 1,
+        scale: 0.3,
+        distance: '0px'
+      }, 700)
+      sr.reveal('.about-text', {
+        duration: 600,
+        viewFactor: .5,
+        scale: 0.3,
+        distance: '0px',
+      }, 200);
+    });
   });
 });
