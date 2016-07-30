e7324d7c6ea640c0fdc82427c30a3918f0126c06
Derick Vigne <derickvigne@me.com>
2016-07-30 00:22:31 -0400

    Added More CSS and Optimized For Mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,13 @@ $(function() {
     );
 
     window.sr = ScrollReveal();
+    sr.reveal('.reveal', {
+      duration: 700,
+      delay: 500,
+      viewFactor: 1,
+      scale: 0.3,
+      distance: '0px'
+    }, 700)
     sr.reveal('.about-text', {
       duration: 600,
       viewFactor: .5,
