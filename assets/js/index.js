6d980c650d92613c205e9d9891db2fd96b506b4f
Derick Vigne <derickvigne@me.com>
2016-07-29 22:26:11 -0400

    Added Jquery Animations and CSS Styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,20 @@
 $(function() {
   $(document).ready(function (e) {
+
     $('h1').fitText(
       1.2, {
         minFontSize: '35px',
         maxFontSize: '65px'
       }
     );
-    
-  })
-})
+
+    window.sr = ScrollReveal();
+    sr.reveal('.about-text', {
+      duration: 600,
+      viewFactor: .5,
+      scale: 0.3,
+      distance: '0px',
+    }, 200);
+
+  });
+});
