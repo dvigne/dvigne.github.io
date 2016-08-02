f156c2b1fdb4c6ad463104f5af41bdbf2c2c195a
Derick Vigne <derickvigne@me.com>
2016-08-02 18:38:44 -0400

    Added Certification and Coloring

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,12 @@
           scale: 0.3,
           distance: '0px',
         }, 200);
+        sr.reveal('.cert_reveal li', {
+          duration: 600,
+          viewFactor: .5,
+          scale: 0.3,
+          distance: '0px',
+        }, 50);
         sr.reveal('.support-text', {
           duration: 600,
           viewFactor: .5,
