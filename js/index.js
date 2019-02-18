05b7bd036d78a57e2136e830da5f2019b6ca7034
Derick Vigne <derickvigne@me.com>
2019-02-17 19:40:51 -0500

    Added JQuery Scrolling Animations

--- js/index.js
+++ js/index.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
       $('nav').removeClass('nav-dark', 400, 'swing');
     }
   });
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 });
 
 ScrollReveal({
