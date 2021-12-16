cba7184944dd35a2ecb2bdbf135db9e19c4a2fad
Derick Vigne <derickvigne@me.com>
2021-12-16 16:30:46 -0500

    Changed Class Names and Added Lazyloading to Animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
+  $('.lazyload').removeClass('d-none'); // Once completely loaded, display hidden elements
+
   $(window).on('scroll', function() {
     if($(window).scrollTop() > 200) {
-      $('nav').addClass('nav-dark', 400, 'swing');
+      $('.nav-transition').addClass('nav-dark', 400, 'swing');
     }
     else {
-      $('nav').removeClass('nav-dark', 400, 'swing');
+      $('.nav-transition').removeClass('nav-dark', 400, 'swing');
     }
   });
+
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -20,44 +23,49 @@ $(document).ready(function() {
       }
     }
   });
-});
 
-ScrollReveal({
-  distance: '100px',
-  viewFactor: .5,
-  duration: 600,
-});
+  ScrollReveal({
+    distance: '100px',
+    viewFactor: .5,
+    duration: 600,
+  });
 
-ScrollReveal().reveal('.reveal-left', {
-  origin: 'left',
-});
-ScrollReveal().reveal('.reveal-right', {
-  origin: 'right',
-});
-ScrollReveal().reveal('.reveal-bottom', {
-  origin: 'bottom',
-});
-ScrollReveal().reveal('.reveal-scale', {
-  distance: '0px',
-  scale: .3
-});
-ScrollReveal().reveal('.reveal-scale-interval', {
-  viewFactor: 1,
-  distance: '0px',
-  scale: .3,
-  interval: 150,
-});
-ScrollReveal().reveal('.reveal-scale-text', {
-  // distance: '0px',
-  // scale: '.3',
-  origin: 'bottom',
-  interval: 400,
-  afterReveal: function() {
-    $.getJSON("https://api.github.com/users/dvigne", function(data) {
-      $("#repositories").html(data['public_repos']);
-      $("#gists").html(data['public_gists']);
-      $("#followers").html(data['followers']);
-      $("#following").html(data['following']);
-    });
-  }
+  ScrollReveal().reveal('.reveal-left', {
+    origin: 'left',
+  });
+  ScrollReveal().reveal('.reveal-right', {
+    origin: 'right',
+  });
+  ScrollReveal().reveal('.reveal-bottom', {
+    origin: 'bottom',
+  });
+  ScrollReveal().reveal('.reveal-scale', {
+    distance: '0px',
+    scale: .3
+  });
+  ScrollReveal().reveal('.reveal-scale-interval', {
+    viewFactor: 1,
+    distance: '0px',
+    scale: .3,
+    interval: 150,
+  });
+  ScrollReveal().reveal('.reveal-scale-text', {
+    // distance: '0px',
+    // scale: '.3',
+    origin: 'bottom',
+    interval: 400,
+    afterReveal: function() {
+      $.getJSON("https://api.github.com/users/dvigne", function(data) {
+        $("#repositories").html(data['public_repos']);
+        $("#gists").html(data['public_gists']);
+        $("#followers").html(data['followers']);
+        $("#following").html(data['following']);
+      });
+    }
+  });
+  ScrollReveal().reveal('.reveal-scale-card', {
+    viewFactor: 0.1,
+    origin: 'bottom',
+    interval: 100,
+  });
 });
